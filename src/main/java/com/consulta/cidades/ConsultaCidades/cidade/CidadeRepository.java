e2f7bddf09c45906490978e97e62bbb4abf52018
unknown <paulomoreira26@hotmail.com>
2021-05-11 16:32:06 -0300

    Criando a entidade Cidade

---
+++ src/main/java/com/consulta/cidades/ConsultaCidades/cidade/CidadeRepository.java
@@ -0,0 +1,6 @@
+package com.consulta.cidades.ConsultaCidades.cidade;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeRepository extends JpaRepository <Cidade, Long>{
+}
